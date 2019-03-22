16943fe707a27a009e63aa63088c693e3bac17f9
William REGNART <william.regnart@igtp4-mac15.isim.intra>
2019-03-22 09:51:04 +0100

    Initial Commit

---
+++ swift_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_project
+//
+//  Created by William REGNART on 22/03/2019.
+//  Copyright © 2019 REGNART-SANCHEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
