8ed9cb1355315c258958a252efbd25031c943633
William REGNART <william.regnart@igtp4-mac15.isim.intra>
2019-03-22 15:41:23 +0100

    add tableviewcontroller for trip

---
+++ swift_project/TripTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TripTableViewCell.swift
+//  swift_project
+//
+//  Created by William REGNART on 22/03/2019.
+//  Copyright © 2019 REGNART-SANCHEZ. All rights reserved.
+//
+
+import UIKit
+
+class TripTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameVoyageLabel: UILabel!
+    @IBOutlet weak var imageLabel: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- swift_project/ViewController.swift
+++ swift_project/ViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
+    @IBOutlet weak var TripTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
