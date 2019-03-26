e9a759f4e09800b4c1534db6d3053308c4feb7c1
DIEGO KRZYZANOWSKI <diegokrzyzanowski@macbook-air-de-diego.local>
2019-03-26 18:38:41 +0100

    fix Person model

--- swift_project/Model/Person.swift
+++ swift_project/Model/Person.swift
@@ -34,6 +34,15 @@ extension Person{
         return result
     }
     
+    var creancesCostPerson: CostPersonSet{
+        let creanceList = CostPersonDAO.getCostPersonbyPerson(person: self)
+        let result = CostPersonSet()
+        for creance in creanceList{
+            result.add(personCost: creance)
+        }
+        return result
+    }
+    
     convenience init(name: String){
         self.init(context: CoreDataManager.context)
         self.name = name
@@ -50,29 +59,20 @@ extension Person{
     //Calcul de la balance de la personne
     //Trip: Voyage pour lequel on calcule la balance de la personne
     //Retourne un double positif si on doit de l'argent à cette personne, négatif si cette personne doit de l'argent
-    func balance(trip : Trip) -> Double {
-        var balance : Double = 0.0
+    var balance:Double{
+        var blc: Double = 0.0
         
         //Parcours de la liste des dépenses pour le voyage
-        for cost in trip.tcostSet {
-            
-            //Parcours de la liste des personnes concernées par la dépense
-            //Si la personne est concernée, on a payé pour elle donc on retire le montant de la dépense divisé par le nombre de personnes concernées de sa balance
-            for person in cost.cpersonsConcerned{
-                if self == person {
-                    balance -= cost.amount / Double(cost.cpersonsConcerned.count)
-                }
-            }
-            //Parcours de la liste des personnes ayant payé pour la dépense
-            //Si la personne a payé, on ajoute le montant qu'elle a payé à sa balance
-            for costPerson in cost.cpersonsWhoPaid{
-                if self == costPerson.cpperson {
-                    balance += costPerson.cpcost
-                }
-            }
+        for cost in self.costs {
+            blc -= cost.amount / Double(cost.cpersonsConcerned.count)
+        }
+        //Parcours de la liste des personnes ayant payé pour la dépense
+        //Si la personne a payé, on ajoute le montant qu'elle a payé à sa balance
+        for creance in self.creancesCostPerson{
+            blc += creance.amount
         }
         
-        return balance
+        return blc
     }
     
 }
