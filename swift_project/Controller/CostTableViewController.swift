dd242da7ded5358cdb5ab217fe0413bf0817b74b
DIEGO KRZYZANOWSKI <williamregnart@gmail.com>
2019-03-26 17:02:35 +0100

    add CostDAO

---
+++ swift_project/Controller/CostTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CostTableViewController.swift
+//  swift_project
+//
+//  Created by DIEGO KRZYZANOWSKI on 26/03/2019.
+//  Copyright © 2019 REGNART-SANCHEZ. All rights reserved.
+//
+
+import UIKit
+
+class CostTableViewController: UIViewController, UITableViewDataSource,UITextFieldDelegate {
+    
+    @IBOutlet weak var creancesTable: UITableViewCell!
+    @IBOutlet weak var costsTable: UITableView!
+    var costs : CostSetViewModel
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ swift_project/ViewModel/CostSetViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  CostSetViewModel.swift
+//  swift_project
+//
+//  Created by DIEGO KRZYZANOWSKI on 26/03/2019.
+//  Copyright © 2019 REGNART-SANCHEZ. All rights reserved.
+//
+
+import Foundation
+
+class CostSetViewModel{
+    
+    var costModel : Cost
+    var costDAO : CostDAO
+}
